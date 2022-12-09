77f2701325874481822a0783d049e83fd061e4bb
MoinKhan <moin@esparkconsultants.com>
2022-12-09 21:00:57 +0500

    PWA Icons Changes and dialog box added in sidebar

--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -8,8 +8,15 @@ import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
 import NightlightRoundIcon from '@mui/icons-material/NightlightRound';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router'
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import TransitionsModal from './modal';
+
 
 export default function Sidebar({handleMode}) {
+  const [open, setOpen] = React.useState(false);
+  const handleClose = () => setOpen(false);
+  const handleOpen = () => setOpen(true);
+
   const navigate = useNavigate()
   return (
     <Box  flex={1} p={2} sx={{display:{xs:"none",sm:"block"}}}> 
@@ -47,7 +54,6 @@ export default function Sidebar({handleMode}) {
               <ListItemText primary="Group" />
             </ListItemButton>
           </ListItem>
-         
           <ListItem disablePadding>
             <ListItemButton component ='a' href='#home'>
               <ListItemIcon>
@@ -58,6 +64,10 @@ export default function Sidebar({handleMode}) {
           </ListItem>
     </List>
     </Box>
+    <Box position={'fixed'} top ="855px" left="125px"   >
+      <AddCircleIcon fontSize= "large" sx={{width:"45px",height:"55px",background:"green",borderRadius:"10px",color:"white",cursor:"pointer"}} onClick={handleOpen}  />
+    </Box>
+    <TransitionsModal handleClose={handleClose} handleOpen={handleOpen} open={open} />
     </Box>
   )
 }
